0f3a285308788d45199b5db913c1d65a016cceab
Nishizawa Shuhei <shuhei.nishizawa@harusoft.onmicrosoft.com>
2019-01-16 10:15:20 +0900

    初回コミット

---
+++ TakeMeThereXamarinForms/TakeMeThereXamarinForms/Models/Geolocation.cs
@@ -0,0 +1,103 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Essentials = Xamarin.Essentials;
+
+
+namespace TakeMeThereXamarinForms.Models
+{
+    interface IGeolocation
+    {
+        double Latitude { get; set; }
+        double Longitude { get; set; }
+        double? Altitude { get; set; }
+        double? Speed { get; set; }
+        double? Cource { get; set; }
+
+
+        void Start(TimeSpan timeSpan);
+        void Stop();
+
+    }
+
+    class Geolocation : BindableBase, IGeolocation
+    {
+        private Essentials.Location _location;
+        public Essentials.Location Location
+        {
+            get => _location;
+            set => SetProperty(ref _location, value);
+        }
+
+        private double _latitude;
+        public double Latitude { get => _latitude; set => SetProperty(ref _latitude, value); }
+
+        private double _longitude;
+        public double Longitude { get => _longitude; set => SetProperty(ref _longitude, value); }
+
+        private double? _altitude;
+        public double? Altitude { get => _altitude; set => SetProperty(ref _altitude, value); }
+
+        private double? _speed;
+        public double? Speed { get => _speed; set => SetProperty(ref _speed, value); }
+
+        private double? _cource;
+        public double? Cource { get => _cource; set => SetProperty(ref _cource, value); }
+
+        private static Geolocation _singletonInstance = new Geolocation();
+        private Geolocation()
+        {
+        }
+        public static Geolocation GetInstance()
+        {
+            return _singletonInstance;
+        }
+
+
+        public event EventHandler OnGetGeolocation;
+
+
+        readonly Essentials.GeolocationRequest request = new Essentials.GeolocationRequest(Essentials.GeolocationAccuracy.Best);
+
+        private async Task UpdateLocationAsync()
+        {
+            this.Location = await Essentials.Geolocation.GetLocationAsync(request);
+
+            this.Latitude = this.Location.Latitude;
+            this.Longitude = this.Location.Longitude;
+            this.Altitude = this.Location.Altitude;
+            this.Speed = this.Location.Speed;
+            this.Cource = this.Location.Course;
+
+            OnGetGeolocation?.Invoke(this, EventArgs.Empty);
+        }
+
+
+
+        private static bool _timerWorking;
+        public void Start(TimeSpan timeSpan)
+        {
+            _timerWorking = true;
+            Device.StartTimer(timeSpan, () =>
+             {
+                 UpdateLocationAsync();
+
+                 return _timerWorking;
+             });
+        }
+
+        public void Stop()
+        {
+            _timerWorking = false;
+        }
+
+
+        public async Task<Essentials.Location> GetCurrentLocationNowAsync()
+        {
+            return await Essentials.Geolocation.GetLocationAsync();
+        }
+    }
+}
---
+++ TakeMeThereXamarinForms/TakeMeThereXamarinForms/Views/MainPage.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+using Color = System.Drawing.Color;
+
+
+namespace TakeMeThereXamarinForms.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly double cycleTime = 2000;
+        private Stopwatch stopwatch = new Stopwatch();
+        private bool pageIsActive;
+        private float t;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            skCanvasViewCompass.PaintSurface += OnCanvasViewCompassPaintSurface;
+            skCanvasViewTargetDirection.PaintSurface += OnCanvasViewTargetDirectionPaintSurface;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            pageIsActive = true;
+            stopwatch.Start();
+
+            Device.StartTimer(TimeSpan.FromMilliseconds(33), () =>
+            {
+                t = (float)(stopwatch.Elapsed.TotalMilliseconds % cycleTime / (cycleTime));
+
+                skCanvasViewTargetDirection.InvalidateSurface();
+
+                if (!pageIsActive)
+                    stopwatch.Stop();
+
+                return pageIsActive;
+            });
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            pageIsActive = false;
+        }
+
+
+        /// <summary>
+        /// 目的地の方角を表示するcanvasの初期設定
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void OnCanvasViewTargetDirectionPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            using (SKPaint strokePaint = new SKPaint())
+            {
+                strokePaint.Style = SKPaintStyle.Stroke;
+                strokePaint.Color = SKColors.LawnGreen;
+                strokePaint.StrokeCap = SKStrokeCap.Square;
+                strokePaint.StrokeWidth = 1;
+
+                //座標変換
+                canvas.Translate(info.Width / 2f, info.Height / 2f);
+                canvas.Scale(Math.Min(info.Width / 200f, info.Height / 200f));
+
+
+                for (int circle = 0; circle < 1; circle++)
+                {
+                    float radius = 9 * (circle + t);
+
+                    strokePaint.Color = new SKColor(0x7c, 0xfc, 0x00, (byte)(255 * (circle == 0 ? (1 - t) : 1)));
+
+                    canvas.DrawCircle(0, -90, radius, strokePaint);
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// 方位を表示するcanvasの初期設定
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void OnCanvasViewCompassPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            using (SKPaint strokePaint = new SKPaint())
+            using (SKPaint textPaint = new SKPaint { Color = SKColors.Black, TextSize = 15 })
+            {
+                strokePaint.Style = SKPaintStyle.Stroke;
+                strokePaint.Color = SKColors.Black;
+                strokePaint.StrokeCap = SKStrokeCap.Square;
+
+
+                //座標変換
+                canvas.Translate(info.Width / 2f, info.Height / 2f);
+                canvas.Scale(Math.Min(info.Width / 200f, info.Height / 200f));
+
+                //テキスト準備
+                SKRect textBounds = new SKRect();
+                string directionText = "N";
+                for (int angle = 0; angle < 360; angle += 6)
+                {
+                    strokePaint.StrokeWidth = 1;
+
+                    var yBaseValue = -70;//y座標の基準値。メモリの長さやテキストの位置に使う
+                    var yEndPoint = yBaseValue - 5;
+
+                    if (angle == 0 || angle == 90 || angle == 180 || angle == 270)
+                    {
+                        //ちょっと長めに
+                        yEndPoint = yBaseValue - 10;
+                        //ちょっと太めに
+                        strokePaint.StrokeWidth = 1.5f;
+
+                        //東西南北
+                        switch (angle)
+                        {
+                            case 0:
+                                directionText = "N";
+                                break;
+                            case 90:
+                                directionText = "E";
+                                break;
+                            case 180:
+                                directionText = "S";
+                                break;
+                            case 270:
+                                directionText = "W";
+                                break;
+                        }
+
+
+                        var textWidth = textPaint.MeasureText(directionText, ref textBounds);
+
+                        canvas.DrawText(directionText, -textWidth / 2f, yBaseValue - 15, textPaint);
+                    }
+
+                    canvas.DrawLine(0, yBaseValue, 0, yEndPoint, strokePaint);
+                    canvas.RotateDegrees(6);
+                }
+            }
+        }
+    }
+}
