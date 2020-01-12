5e0d75708cd75cfb355face7b1204c9ca7e988d7
shuheydev <shuhey.dev@gmail.com>
2020-01-12 17:09:40 +0900

    Add ShadowFrame and ShadowFrameRenderer.

---
+++ TakeMeThereXamarinForms/TakeMeThereXamarinForms.Android/Renderers/ShadowFrameRenderer.cs
@@ -0,0 +1,49 @@
+﻿
+using Android.Content;
+using Android.Support.V4.View;
+using CustomRenderer.Droid.Renderers;
+using System.ComponentModel;
+using TakeMeThereXamarinForms.Controls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(ShadowFrame), typeof(ShadowFrameRenderer))]
+namespace CustomRenderer.Droid.Renderers
+{
+    public class ShadowFrameRenderer : Xamarin.Forms.Platform.Android.AppCompat.FrameRenderer
+    {
+        public ShadowFrameRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Frame> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement == null)
+            {
+                return;
+            }
+
+            UpdateElevation();
+        }
+
+        private void UpdateElevation()
+        {
+            var shadowFrame = (ShadowFrame)Element;
+
+            Control.StateListAnimator = new Android.Animation.StateListAnimator();
+
+            ViewCompat.SetElevation(this, shadowFrame.Elevation);
+            ViewCompat.SetElevation(Control, shadowFrame.Elevation);
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            if (e.PropertyName == "Elevation")
+            {
+                UpdateElevation();
+            }
+        }
+    }
+}
---
+++ TakeMeThereXamarinForms/TakeMeThereXamarinForms/Controls/ShadowFrame.cs
@@ -0,0 +1,25 @@
+﻿
+using Xamarin.Forms;
+
+namespace TakeMeThereXamarinForms.Controls
+{
+    public class ShadowFrame : Frame
+    {
+        public float Elevation
+        {
+            get { return (float)GetValue(ElevationProperty); }
+            set { SetValue(ElevationProperty, value); }
+        }
+        public static readonly BindableProperty ElevationProperty = BindableProperty.Create(
+               propertyName: nameof(Elevation),
+               returnType: typeof(float),
+               declaringType: typeof(ShadowFrame),
+               defaultValue: 4.0f,
+               defaultBindingMode: BindingMode.OneWay,
+               propertyChanged: ElevationPropertyChanged
+            );
+        private static void ElevationPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+        }
+    }
+}
