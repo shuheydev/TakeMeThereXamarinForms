b7e5613e70df592d0918082ec7ed49fc96c2f0ce
shuheydev <shuhey.dev@gmail.com>
2020-01-13 15:54:54 +0900

    フォントを導入した.

---
+++ TakeMeThereXamarinForms/TakeMeThereXamarinForms/Resources/IconFont.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TakeMeThereXamarinForms.Resources
+{
+	public static class IconFont
+	{
+		public const string Plus = "\ue81c";
+		public const string Edit = "\ue835";
+		public const string TrashEmpty = "\ue83e";
+		public const string LeftBig = "\ue861";
+		public const string Floppy = "\ue89e";
+		public const string MapO = "\uf278";
+	}
+}
